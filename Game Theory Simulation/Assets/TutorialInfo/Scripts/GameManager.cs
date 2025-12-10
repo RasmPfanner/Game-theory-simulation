1da8d33ecaada8ad577ec5d6b45fbeffbe4b0846
RasmPfanner <rasmpfanner@gmail.com>
2025-12-10 13:36:34 +0100

    Add UI integration for payoff matrix input
    Introduced UIrun script to handle payoff matrix input via IntegerFields and a confirm button in the UI. Updated GameManager to store individual payoff values for both players. Modified GameManagerUI.uxml to use distinct IntegerField names for each payoff and added a confirm button. Added recovery Unity scene files.

--- Game Theory Simulation/Assets/TutorialInfo/Scripts/GameManager.cs
+++ Game Theory Simulation/Assets/TutorialInfo/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour // monobehavior så det kan attaches til et GameObjekt
 {
@@ -8,4 +9,20 @@ public class GameManager : MonoBehaviour // monobehavior så det kan attaches til
     private PayoffMatrix matrix;
     private int currentRound;
     private int maxRounds;
+
+
+    // UI fields for payoff matrix
+
+    // spiller 1
+    private int s1_s1_p1;
+    private int s1_s2_p1;
+    private int s2_s1_p1;
+    private int s2_s2_p1;
+
+    // spiller 2
+    private int s1_s1_p2;
+    private int s1_s2_p2;
+    private int s2_s1_p2;
+    private int s2_s2_p2;
 }
+
